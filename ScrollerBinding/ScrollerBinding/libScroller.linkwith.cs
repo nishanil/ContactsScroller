f4aa0300499759034c2452392f6f2a75d7e3ab23
nishanil <nnish@live.com>
2014-01-29 23:36:56 +0530

    Added AudioToolBox Framework to work with devices

--- ScrollerBinding/ScrollerBinding/libScroller.linkwith.cs
+++ ScrollerBinding/ScrollerBinding/libScroller.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libScroller.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV6 , ForceLoad = true)]
+[assembly: LinkWith ("libScroller.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV6 , Frameworks = "AudioToolbox", ForceLoad = true)]
